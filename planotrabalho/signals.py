4253d76a5610ac82e25145231afdf3ac5993a88c
Gabriel Avaloni <gabrielavaloni@gmail.com>
2019-04-09 16:35:17 -0300

    COrrige testes para a nova funcionalidade de editar diligências

--- planotrabalho/signals.py
+++ planotrabalho/signals.py
@@ -7,12 +7,13 @@ from planotrabalho.models import Componente
 from planotrabalho.models import ConselhoDeCultura
 from planotrabalho.models import FundoDeCultura
 
+
 @receiver(post_save, sender=ArquivoComponente2)
 @receiver(post_save, sender=Componente)
 @receiver(post_save, sender=ConselhoDeCultura)
 @receiver(post_save, sender=FundoDeCultura)
 def inclui_arquivo_no_historico_diligencia(sender, instance, **kwargs):
-    if "diligencia" in kwargs['update_fields']:
+    if kwargs['update_fields'] and "diligencia" in kwargs['update_fields']:
         url = instance.arquivo.url
         ultimo_historico = instance.diligencia.history.latest()
         ultimo_historico.arquivo_url = url
