1ff164b59f31e60f86a5aebc5ec754bcefe522ae
Janilson <janilson.mendes@gmail.com>
2019-10-16 13:57:10 -0300

    [UPDATE]
    - Ctismart#51519: Criação dos ícones para situação dos entes federados.

--- gestao/views.py
+++ gestao/views.py
@@ -341,8 +341,8 @@ class DetalharEnte(DetailView, LookUpAnotherFieldMixin):
         # Situações do Ente Federado
         context[
             'has_analise_nao_correcao'] = sistema.has_not_diligencias_enviadas_aprovadas() and has_legislacao_concluido and has_plano_concluido and has_conselho_concluido and has_fundo_cultura_concluido and has_orgao_gestor_concluido
-        context['has_prazo_vencido'] = self.get_valida_prazo_vencido(
-            sistema) and has_legislacao_arquivo and has_plano_arquivo and has_fundo_cultura_arquivo and has_conselho_lei_arquivo and has_orgao_gestor_arquivo and has_conselho_arquivo and has_comprovante_cnpj_arquivo
+        context['has_prazo_vencido'] = self.get_valida_prazo_vencido(sistema)
+        # and has_legislacao_arquivo and has_plano_arquivo and has_fundo_cultura_arquivo and has_conselho_lei_arquivo and has_orgao_gestor_arquivo and has_conselho_arquivo and has_comprovante_cnpj_arquivo
 
         context['has_pendente_analise'] = (has_legislacao_arquivo and not has_legislacao_concluido) or (
             has_fundo_cultura_arquivo and not has_fundo_cultura_concluido) or (
@@ -373,10 +373,10 @@ class DetalharEnte(DetailView, LookUpAnotherFieldMixin):
     def get_valida_arquivo_concluido(self, field):
         return self.get_valida_arquivo(field) and field.situacao in (2, 3)
 
-    def get_valida_prazo_vencido(self, sistema):
+    def get_valida_prazo_vencido(self, sistema, ano=2):
         data_final_publicacao_acordo = None
         if not sistema.conferencia_nacional and sistema.data_publicacao_acordo is not None:
-            data_final_publicacao_acordo = date(sistema.data_publicacao_acordo.year + 2,
+            data_final_publicacao_acordo = date(sistema.data_publicacao_acordo.year + ano,
                                                 sistema.data_publicacao_acordo.month,
                                                 sistema.data_publicacao_acordo.day)
 
