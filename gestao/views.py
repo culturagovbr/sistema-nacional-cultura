adacb09ff083417cc49a6aecf5e121432f56216b
gabriel.cabral <cabraldealmeida@gmail.com>
2020-04-01 17:10:38 -0300

    ajuste na busca

--- gestao/views.py
+++ gestao/views.py
@@ -1189,10 +1189,12 @@ class DataTableEntes(BaseDatatableView):
 
             for id in pendente_componentes_search:
                 nome_componente = componentes.get(int(id))
+                # kwargs_exclude = {'{0}__situacao__in'.format(nome_componente): [
+                #    0, 2, 3, 4, 5, 6]}
                 kwargs_pendentes = {'{0}__situacao__in'.format(nome_componente): [1]}
                 kwargs_arquivos = {'{0}__arquivo'.format(nome_componente): ''}
-                qs = qs.filter(**kwargs_pendentes).exclude(
-                    **kwargs_arquivos)
+                qs = qs.filter(
+                    **kwargs_pendentes).exclude(**kwargs_arquivos)
 
         if situacao_componentes_search:
             componentes = {
