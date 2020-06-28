f3a4bb5e127182edc074186f0f696c8649ec7fa0
Jose Augusto <joseaugusto.881@outlook.com>
2020-06-28 19:34:42 -0300

    renomando funcoes corretamente agora

--- adesao/views.py
+++ adesao/views.py
@@ -692,7 +692,7 @@ def validate_cnpj(request):
     return JsonResponse(data)
 
 
-def buscacnpj(request):
+def search_cnpj(request):
     """
     Função que busca o cnpj informado na base do infoconv e devolve para o template
     :return: json data
