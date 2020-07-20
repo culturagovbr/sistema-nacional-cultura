93e43dbdb0a1253dcd2716e32d1a8c2023d1e424
rafinharamos <44729436+rafinharamos@users.noreply.github.com>
2020-07-20 10:14:24 -0300

    Revert "adesao"

--- adesao/views.py
+++ adesao/views.py
@@ -37,7 +37,6 @@ from adesao.models import (
     Funcionario,
     EnteFederado,
     Sede,
-    SolicitacaoDeAdesao,
     TrocaCadastrador
 )
 
@@ -770,25 +769,3 @@ class TrocaCadastrador(CreateView):
         else:
             context['form_sistema'] = TrocaCadastradorForm()
         return context
-
-@login_required
-def sucesso_solicitar_adesao(request):
-    return render(request, "mensagem_sucesso_solicitar_adesao.html")
-
-
-class SolicitarAdesaoView(CreateView):
-    template_name = "solicitar_adesao.html"
-    model = SolicitacaoDeAdesao
-    fields = ['oficio']
-    success_url = reverse_lazy("adesao:sucesso_troca_cadastrador")
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        cod_ibge = self.request.POST.get('ente_federado')
-        print(cod_ibge)
-        instance = EnteFederado.objects.get(cod_ibge=cod_ibge)
-        print(instance)
-        self.object.ente_federado = instance
-        self.object.save()
-        return super(ModelFormMixin, self).form_valid(form)
-
